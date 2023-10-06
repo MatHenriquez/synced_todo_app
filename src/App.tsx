cf0d0885aadda27ec460fb9a1126b62e430ae406
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-06 17:04:23 -0300

    basic todo app with real time changes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSyncedStore } from "@syncedstore/react";
+import { store } from "./store";
 
-function App() {
+export default function App() {
+  const state = useSyncedStore(store);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Todo items:</p>
+      <ul>
+        {state.todos.map((todo, i) => {
+          return (
+            <li key={i} style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+              <label>
+                <input type="checkbox" checked={todo.completed} onClick={() => (todo.completed = !todo.completed)} />
+                {todo.title}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+      <input
+        placeholder="Enter a todo item and hit enter"
+        type="text"
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            const target = event.target as HTMLInputElement;
+            // Add a todo item using the text added in the textfield
+            state.todos.push({ completed: false, title: target.value });
+            target.value = "";
+          }
+        }}
+        style={{ width: "200px", maxWidth: "100%" }}
+      />
     </div>
   );
 }
 
-export default App;
